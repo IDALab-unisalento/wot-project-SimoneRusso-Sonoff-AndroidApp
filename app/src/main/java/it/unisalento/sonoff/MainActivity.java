58a26be263421a144d9d079ed297519716b08194
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-11-09 17:59:53 +0100

    Fase 4 semi-completa
    Non riesco ad aggiornare lo stato del dispositivo nella MainActivity dopo aver ricevuto la notifica di cambio di stato

--- app/src/main/java/it/unisalento/sonoff/MainActivity.java
+++ app/src/main/java/it/unisalento/sonoff/MainActivity.java
@@ -3,13 +3,15 @@ package it.unisalento.sonoff;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     Switch lockSwitch;
     Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +20,16 @@ public class MainActivity extends AppCompatActivity {
 
         lockSwitch = findViewById(R.id.lockSwitch);
         button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
 
         RestService restService = new RestService(getApplicationContext());
+        restService.saveToken("prova");
         restService.getStatus(this.lockSwitch);
 
-        Listener listener = new Listener(getApplicationContext());
+
+        Listener listener = new Listener(getApplicationContext(), textView);
 
         lockSwitch.setOnCheckedChangeListener(listener);
         button.setOnClickListener(listener);
-
     }
-
 }
