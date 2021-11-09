77473d9501877a9c35fd8351c38d57f2e8d8b9b6
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-11-09 16:17:23 +0100

    Fase 1 completata e funzionante

---
+++ app/src/main/java/it/unisalento/sonoff/Listener.java
@@ -0,0 +1,21 @@
+package it.unisalento.sonoff;
+
+import android.widget.CompoundButton;
+
+public class Listener implements CompoundButton.OnCheckedChangeListener {
+
+    MQTTHelper mqttHelper;
+    public Listener(MQTTHelper mqttHelper) {
+        this.mqttHelper = mqttHelper;
+    }
+
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        if(isChecked && compoundButton.isPressed())
+            mqttHelper.publish("cmnd/tasmota_8231A8/POWER1", "ON");
+        else if( !isChecked && compoundButton.isPressed())
+            mqttHelper.publish("cmnd/tasmota_8231A8/POWER1", "OFF");
+
+    }
+}
