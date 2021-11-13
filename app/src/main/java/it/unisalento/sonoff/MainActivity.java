20d7b0687ae20a2b7a740a550a9280c271e22dd2
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-11-13 12:51:48 +0100

    Correzione bug

--- app/src/main/java/it/unisalento/sonoff/MainActivity.java
+++ app/src/main/java/it/unisalento/sonoff/MainActivity.java
@@ -3,6 +3,7 @@ package it.unisalento.sonoff;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     Switch lockSwitch;
     Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,12 @@ public class MainActivity extends AppCompatActivity {
 
         lockSwitch = findViewById(R.id.lockSwitch);
         button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
 
         RestService restService = new RestService(getApplicationContext());
         restService.getStatus(this.lockSwitch);
 
-        Listener listener = new Listener(getApplicationContext());
+        Listener listener = new Listener(getApplicationContext(), textView);
 
         lockSwitch.setOnCheckedChangeListener(listener);
         button.setOnClickListener(listener);
