dd72715faa90771b7cc6c3591f1a776860f83405
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2022-03-02 18:13:18 +0100

    Rest call to Log Event

--- app/src/main/java/it/unisalento/sonoff/model/Event.java
+++ app/src/main/java/it/unisalento/sonoff/model/Event.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Event {
     private String eventType;
     private String user;
-    private Date date;
+    private String date;
 
     public String getEventType() {
         return eventType;
@@ -23,11 +23,15 @@ public class Event {
         this.user = user;
     }
 
-    public Date getDate() {
+    public String getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setDate(String date) {
+        String[] temp = date.replace("T", " ").substring(0,19).split(" ");
+        String y = temp[0].split("-")[0];
+        String m = temp[0].split("-")[1];
+        String d = temp[0].split("-")[2];
+        this.date = d+"-"+m+"-"+y+", "+temp[1];
     }
 }
